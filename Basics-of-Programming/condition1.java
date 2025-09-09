1387cdd1517df643477d3d84e18e85c34d4e70e5
Chandrashekar0123 <chandrashekar63707@gmail.com>
2025-09-09 12:41:27 +0530

    day 2

---
+++ Basics-of-Programming/condition1.java
@@ -0,0 +1,11 @@
+class condition1{
+    public static void main(String[] args) {
+        int age = 18;
+        if(age >= 18) {
+                 System.out.print("Have access");
+        }
+        else {
+            System.out.print("Dont have access");
+        }
+    }
+}
---
+++ Basics-of-Programming/elseifladder.py
@@ -0,0 +1,10 @@
+a = 20
+b = 50
+if a == b:
+    print('equal')
+elif b>a:
+    print('B is Greater')
+elif a>b:
+    print('A is Greater')        
+else :
+    print('Invalid Comparision')    
